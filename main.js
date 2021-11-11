11c9fe40af5be2d812a3de723aeb325d67efbe9c
hunseok-Y <gnstjr5911@gmail.com>
2021-11-12 01:52:36 +0900

    html수정 및 css 수정
    html수정 및 css 수정

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js";
+document.addEventListener("DOMContentLoaded", () => {
+  const mainPgeSwiper = new Swiper(".main-page-swiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    autoHeight: true,
+    nested: true,
+    observer: true,
+    observeParents: true,
+    delay: 3000,
+    pagination: {
+      el: ".main-page__pagination",
+      clickable: true,
+    },
+  });
 
-const swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  slidesPerView: 1,
-  spaceBetween: 30,
-  mousewheel: true,
-  autoHeight: true,
-  delay: 3000,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  const ItemList = new Swiper(".item-list", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    nested: true,
+    observer: true,
+    observeParents: true,
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".list-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".list-button-next",
+      prevEl: ".list-button-prev",
+    },
+  });
 });
