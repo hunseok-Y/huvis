a4d3a07facb6ddca5956f7532efb1333512dda3b
hunseok-Y <gnstjr5911@gmail.com>
2021-11-19 15:10:21 +0900

    htmh수정 및 css 수정
    htmh수정 및 css 수정

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js";
 document.addEventListener("DOMContentLoaded", () => {
+  const bullet = [
+    "Main",
+    "Products",
+    "About us",
+    "IR & Career",
+    "PR Center",
+    "Life in Huvis",
+  ];
   const mainPgeSwiper = new Swiper(".main-page-swiper", {
     direction: "vertical",
     slidesPerView: 1,
@@ -9,17 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
     nested: true,
     observer: true,
     observeParents: true,
-    speed: 2000,
+    speed: 1000,
     delay: 3000,
     pagination: {
       el: ".main-page__pagination",
       clickable: true,
+      renderBullet: function (index, className) {
+        return (
+          '<div class="' +
+          className +
+          '"><span>' +
+          bullet[index] +
+          "</span></div>"
+        );
+      },
     },
   });
 
   const ItemList = new Swiper(".item-list", {
-    spaceBetween: 30,
+    spaceBetween: 0,
     centeredSlides: true,
+    autoHeight: true,
     nested: true,
     observer: true,
     observeParents: true,
@@ -40,5 +58,24 @@ document.addEventListener("DOMContentLoaded", () => {
       nextEl: ".list-button-next",
       prevEl: ".list-button-prev",
     },
+  });
+
+  new Swiper(".products__swiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    nested: true,
+    loop: true,
+  });
+
+  const button = document.querySelector(".family__button");
+  button.addEventListener("click", () => {
+    const dropdown = document.querySelector(".family__discroption");
+    button.classList.add("active");
+  });
+
+  button.addEventListener("blur", () => {
+    const dropdown = document.querySelector(".family__discroption");
+    dropdown.classList.remove("active");
   });
 });
